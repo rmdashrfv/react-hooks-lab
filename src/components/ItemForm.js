55ceb5aca7ba73d442efe8901a5769f47921bd1b
Michael Law <rmdashrfv@hey.com>
2022-07-21 18:25:28 -0400

    lab solution

---
+++ src/components/ItemForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+function ItemForm(props) {
+  const [itemName, setItemName] = useState('')
+  const [itemCategory, setItemCategory] = useState('')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newItem = {id: uuid(), name: itemName, category: itemCategory}
+    console.log('new item', newItem)
+    props.onItemFormSubmit(newItem)
+  }
+
+  return (
+    <form className="NewItem" onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" onChange={(e) => {setItemName(e.target.value) }} />
+      </label>
+
+      <label>
+        Category:
+        <select name="category" onChange={(e) => {setItemCategory(e.target.value) }}>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </label>
+
+      <button type="submit">Add to List</button>
+    </form>
+  );
+}
+
+export default ItemForm;
---
+++ src/components/ShoppingList.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ItemForm from "./ItemForm";
+import Filter from "./Filter";
+import Item from "./Item";
+import itemData from '../data/items'
+
+function ShoppingList() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [items, setItems] = useState(itemData)
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  const onItemFormSubmit = (newItem) => {
+    setItems([...items, newItem])
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
+
+  return (
+    <div className="ShoppingList">
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} />
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ShoppingList;
